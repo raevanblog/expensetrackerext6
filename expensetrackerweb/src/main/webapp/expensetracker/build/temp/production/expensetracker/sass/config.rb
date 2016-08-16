0f9e61176fa22db74bea0ac6861c1b86b30a91d7
Shyam N <Shyam N>
2016-08-16 08:33:12 +0530

    Expense Tracker EXT-JS 6 Development

---
+++ expensetrackerweb/src/main/webapp/expensetracker/build/temp/production/expensetracker/sass/config.rb
@@ -0,0 +1,2 @@
+Compass.add_project_configuration('..\..\..\..\..\sass\config.rb')
+cache_path = 'G:\extjs6-new-development\expensetracker\expensetrackerweb\src\main\webapp\expensetracker/build/.sass-cache'
---
+++ expensetrackerweb/src/main/webapp/expensetracker/build/temp/production/expensetracker/sencha-compiler/app/full-page-master-bundle.js
@@ -0,0 +1,2 @@
+// @tag full-page
+// @require G:\extjs6-new-development\expensetracker\expensetrackerweb\src\main\webapp\expensetracker\app.js
