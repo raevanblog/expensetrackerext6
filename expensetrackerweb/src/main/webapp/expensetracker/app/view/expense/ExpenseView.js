ca0d6d879cc92449450e7fd5d551f7326488ef81
shyam <shyam@SHYAM-PC>
2016-09-12 20:48:58 +0530

    added requires

--- expensetrackerweb/src/main/webapp/expensetracker/app/view/expense/ExpenseView.js
+++ expensetrackerweb/src/main/webapp/expensetracker/app/view/expense/ExpenseView.js
@@ -2,7 +2,8 @@ Ext.define('expensetracker.view.expense.ExpenseView', {
 	extend : 'Ext.container.Container',
 	xtype : 'expenseview',
 	alias : 'view.expenseview',
-	requires : [ 'expensetracker.view.expense.ExpenseDock', 'expensetracker.view.expense.ExpenseViewController' ],
+	requires : [ 'expensetracker.view.expense.ExpenseDock', 'expensetracker.view.expense.ExpenseViewController', 'Ext.form.FieldSet',
+			'Ext.form.FieldContainer', 'Ext.form.SliderField' ],
 	controller : 'expenseviewcontroller',
 	layout : {
 		type : 'vbox',
@@ -17,37 +18,37 @@ Ext.define('expensetracker.view.expense.ExpenseView', {
 			xtype : 'fieldcontainer',
 			layout : {
 				type : 'hbox',
-				padding: '0 0 0 15'
-			},			
+				padding : '0 0 0 15'
+			},
 			items : [ {
 				xtype : 'component',
 				reference : 'expviewselyear',
-				html: '',	
-				cls: 'year-square'			
+				html : '',
+				cls : 'year-square'
 			}, {
 				xtype : 'sliderfield',
-				fieldLabel : 'Select Year',				
+				fieldLabel : 'Select Year',
 				labelSeparator : '',
 				value : 2016,
-				padding: '20 10 0 10',
+				padding : '20 10 0 10',
 				increment : 1,
 				minValue : 1990,
 				maxValue : 2016,
 				flex : 0.80,
-				platformConfig: {
-					'!desktop': {
-						width: '100%'
+				platformConfig : {
+					'!desktop' : {
+						width : '100%'
 					}
 				},
-				listeners: {
-					changecomplete: 'onYearSelection',
-					afterrender: 'onRenderSlider'
+				listeners : {
+					changecomplete : 'onYearSelection',
+					afterrender : 'onRenderSlider'
 				}
-			},{
-				xtype: 'button',				
+			}, {
+				xtype : 'button',
 				flex : 0.10,
-				margin: '20 0 0 0',
-				text: 'Open Book'
+				margin : '20 0 0 0',
+				text : 'Open Book'
 			} ]
 		} ]
 	}, {
