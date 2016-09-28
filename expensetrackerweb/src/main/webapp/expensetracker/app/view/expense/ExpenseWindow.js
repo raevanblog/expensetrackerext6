6fa1f7956119ffe4d9eb8afbde08e429115f8036
shyam <shyam@SHYAM-PC>
2016-09-28 09:37:15 +0530

    Added Expense Window

---
+++ expensetrackerweb/src/main/webapp/expensetracker/app/view/expense/ExpenseWindow.js
@@ -0,0 +1,38 @@
+Ext.define('expensetracker.view.expense.ExpenseWindow', {
+	extend : 'Ext.window.Window',
+	alias : 'view.expensewindow',
+	xtype : 'expensewindow',
+	controller : 'expensewindowcontroller',
+	viewModel : 'expensewindowmodel',
+	requires : [ 'expensetracker.view.expense.ExpenseWindowController', 'expensetracker.view.expense.ExpenseWindowModel',
+			'expensetracker.view.expense.ExpenseGrid', 'expensetracker.view.expense.ExpenseCategory' ],
+	layout : {
+		type : 'card',
+		animate : true,
+		animation : {
+			type : 'pop',
+			easing : 'ease-in',			
+			direction : 'left'
+		}
+	},
+	listeners : {
+		beforeclose : 'onCloseExpenseWindow'
+	},
+	items : [ {
+		xtype : 'expensegrid',
+		reference : 'expensegrid',
+		store : Ext.create('expensetracker.store.Expense'),
+		listeners : {
+			afterrender : 'onRenderExpenseGrid'
+		}
+	}, {
+		xtype : 'expensecategory',
+		reference : 'expensecategory',
+		tbar : [ '->', {
+			xtype : 'button',
+			text : 'Back',
+			iconCls : 'x-fa fa-backward',
+			handler : 'onBackCategory'
+		} ]
+	} ]
+});
