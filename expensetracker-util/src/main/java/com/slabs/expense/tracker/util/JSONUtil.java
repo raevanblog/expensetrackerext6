658a89b73159b9300505a0c1ffa4f5a42485ac33
shyam <shyam@SHYAM-PC>
2016-11-08 20:37:11 +0530

    Added java doc

--- expensetracker-util/src/main/java/com/slabs/expense/tracker/util/JSONUtil.java
+++ expensetracker-util/src/main/java/com/slabs/expense/tracker/util/JSONUtil.java
@@ -2,6 +2,7 @@ package com.slabs.expense.tracker.util;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
@@ -24,12 +25,24 @@ public class JSONUtil {
 	 * @throws IOException
 	 *             throws {@link IOException}
 	 */
+	@SuppressWarnings("unchecked")
 	public static String getJSONString(Object object) throws IOException {
 
 		ObjectMapper mapper = new ObjectMapper();
 		return mapper.writeValueAsString(object);
 	}
 
+	/**
+	 * This method will return a map of key/value pairs from the JSON in the
+	 * {@link InputStream}
+	 * 
+	 * @param is
+	 *            {@link InputStream} - Input Stream from which the JSON string
+	 *            will be processed
+	 * @return {@link Map} - Map of key/value pairs
+	 * @throws IOException
+	 *             throws {@link IOException}
+	 */
 	@SuppressWarnings("unchecked")
 	public static Map<String, String> getMapFromInputStream(InputStream is) throws IOException {
 
@@ -37,6 +50,16 @@ public class JSONUtil {
 		return mapper.readValue(is, Map.class);
 	}
 
+	/**
+	 * This method will return a map of key/value from the JSON string
+	 * 
+	 * @param is
+	 *            {@link InputStream} - Input Stream from which the JSON string
+	 *            will be processed
+	 * @return {@link Map} - Map of key/value pairs
+	 * @throws IOException
+	 *             throws {@link IOException}
+	 */
 	@SuppressWarnings("unchecked")
 	public static Map<String, String> getMapFromString(String is) throws IOException {
 
@@ -44,19 +67,40 @@ public class JSONUtil {
 		return mapper.readValue(is, Map.class);
 	}
 
+	/**
+	 * This method will return a map of key/value pairs from the given
+	 * {@link Object}
+	 * 
+	 * @param object
+	 *            {@link Object} - Input Stream from which the JSON string will
+	 *            be processed
+	 * @return {@link Map} - Map of key/value pairs
+	 * @throws IOException
+	 *             throws {@link IOException}
+	 */
 	@SuppressWarnings("unchecked")
-	public static Map<String, String> getMapFromObject(Object is) throws IOException {
+	public static Map<String, String> getMapFromObject(Object object) throws IOException {
 
 		ObjectMapper mapper = new ObjectMapper();
-		return mapper.readValue(getJSONString(is), Map.class);
+		return mapper.readValue(getJSONString(object), Map.class);
 	}
 
-	public static <T extends Object> List<T> getListFromJSON(String json, Class<T> cls)
-			throws IOException {
+	/**
+	 * This method will return a {@link Collection} based on the given
+	 * collection class type
+	 * 
+	 * @param json
+	 *            {@link String} - JSON string
+	 * @param cls
+	 *            {@link Class} - Any class that implements {@link Collection}
+	 * @return {@link Collection} - Collection containing JSON key/value
+	 * @throws IOException
+	 *             throws {@link IOException}
+	 */
+	public static <T extends Object> List<T> getListFromJSON(String json, Class<T> cls) throws IOException {
 
 		ObjectMapper mapper = new ObjectMapper();
-		return mapper.readValue(json,
-				mapper.getTypeFactory().constructCollectionType(List.class, cls));
+		return mapper.readValue(json, mapper.getTypeFactory().constructCollectionType(List.class, cls));
 	}
 
 }
