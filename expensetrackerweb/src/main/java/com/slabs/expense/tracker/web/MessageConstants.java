cc6c06a437a17dfd19f4ae4a593700ba5aabe9d7
tkmaed4 <tkmaed4@VCFCVIEW7982.cp.ad.kohls.com>
2016-11-16 18:06:02 +0530

    Added MessageConstants

---
+++ expensetrackerweb/src/main/java/com/slabs/expense/tracker/web/MessageConstants.java
@@ -0,0 +1,28 @@
+package com.slabs.expense.tracker.web;
+
+public class MessageConstants {
+
+	public static final String EXCEPTION = "* Exception occurred, please contact customer support";
+
+	public static final String CHECK_USRNME_PWD = "* Please check your username/password";
+
+	public static final String CHECK_PWD = "* Please check your password";
+
+	public static final String LOGIN_SUCCESS = "Login Successful";
+
+	public static final String INVALID_SESSION = "Session Invalid";
+
+	public static final String SERVER_ERROR = "* Server Error";
+
+	public static final String AVAILABLE = "Available";
+
+	public static final String NOT_AVAILABLE = "Not Available";
+
+	public static final String LOGD_OFF = "Logged Off";
+
+	public static final String SESSION_ACTIVE = "Session Active";
+
+	public static final String USER_REGISTERED = "User registered successfully";
+
+	public static final String USER_NOT_REGISTERED = "User registration failed. Please contact customer support";
+}
--- expensetrackerweb/src/main/java/com/slabs/expense/tracker/web/WebConstants.java
+++ expensetrackerweb/src/main/java/com/slabs/expense/tracker/web/WebConstants.java
@@ -8,6 +8,8 @@ public class WebConstants {
 	
 	public static final String MESSAGE = "message";
 	
+	public static final String IS_USER_REGISTERED = "isUserRegistered";
+	
 	public static final String JSON = "json";
 	
 	public static final String USER = "user";
