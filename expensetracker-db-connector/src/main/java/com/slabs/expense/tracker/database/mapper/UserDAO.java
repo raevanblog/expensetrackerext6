4dc7e496dbc8f6aa4f504bdd24d839655bed9006
shyam <shyam@SHYAM-PC>
2016-12-07 08:08:31 +0530

    Updated mapper methods for updating activation code

--- expensetracker-db-connector/src/main/java/com/slabs/expense/tracker/database/mapper/UserDAO.java
+++ expensetracker-db-connector/src/main/java/com/slabs/expense/tracker/database/mapper/UserDAO.java
@@ -4,8 +4,8 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Param;
 
+import com.slabs.expense.tracker.common.db.entity.Feature;
 import com.slabs.expense.tracker.common.db.entity.Message;
-import com.slabs.expense.tracker.common.db.entity.UserFeature;
 import com.slabs.expense.tracker.common.db.entity.UserInfo;
 
 /**
@@ -17,8 +17,7 @@ import com.slabs.expense.tracker.common.db.entity.UserInfo;
  */
 public interface UserDAO {
 
-	public List<UserInfo> getUser(@Param("username") String username,
-			@Param("includePassword") boolean includePassword) throws Exception;
+	public List<UserInfo> getUser(@Param("username") String username, @Param("includePassword") boolean includePassword) throws Exception;
 
 	public Integer createUser(@Param("record") UserInfo record) throws Exception;
 
@@ -26,20 +25,21 @@ public interface UserDAO {
 
 	public Integer updateUser(@Param("record") UserInfo record) throws Exception;
 
-	public Integer insertUserFeature(@Param("record") UserFeature feature) throws Exception;
+	public Integer insertUserFeature(@Param("record") Feature feature) throws Exception;
 
-	public Integer updateUserFeature(@Param("record") UserFeature feature) throws Exception;
+	public Integer updateUserFeature(@Param("record") Feature feature) throws Exception;
+
+	public Integer setActivationKey(@Param("username") String username, @Param("activationKey") String activationKey) throws Exception;
 
 	public Integer deleteUserFeature(@Param("username") String username) throws Exception;
 
-	public UserFeature getUserFeature(@Param("username") String username) throws Exception;
+	public Feature getUserFeature(@Param("username") String username) throws Exception;
 
 	public Integer insertMessage(@Param("records") List<Message> records) throws Exception;
 
 	public Integer updateMessage(@Param("record") Message message) throws Exception;
 
-	public Integer deleteMessage(@Param("record") Message message,
-			@Param("isDeleteOldMsg") boolean isDeleteOldMsg) throws Exception;
+	public Integer deleteMessage(@Param("record") Message message, @Param("isDeleteOldMsg") boolean isDeleteOldMsg) throws Exception;
 
 	public List<Message> getMessage(@Param("username") String username) throws Exception;
 
