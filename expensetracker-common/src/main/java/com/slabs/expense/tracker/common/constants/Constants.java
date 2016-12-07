78be455c4f764358af99f3e08285fb6c8a420e94
shyam <shyam@SHYAM-PC>
2016-12-07 08:07:37 +0530

    Added String constants for common messages

---
+++ expensetracker-common/src/main/java/com/slabs/expense/tracker/common/constants/Constants.java
@@ -0,0 +1,25 @@
+package com.slabs.expense.tracker.common.constants;
+
+public class Constants {
+
+	public static final String APP_NAME = "expensetracker";
+
+	public static final String MAIL_PROPERTIES = "email.properties";
+
+	public static final String YES = "YES";
+
+	public static final String Y = "Y";
+
+	public static final String NO = "NO";
+
+	public static final String N = "N";
+
+	public static final String EMAIL_ACTIVATION_TEMPLATE = "activate.ftl";
+
+	public static final String REG_SUCCESS_TEMPLATE = "registration_success.ftl";
+
+	public static final String EMAIL_ACTIVATION_SUBJECT = "Activate your Account";
+
+	public static final String EMAIL_ACTIVATION_SUCCESS_SUBJECT = "Account Activated";
+
+}
