0a3a94622b8d5d5c2b15e18a58844fdc7091408b
shyam <shyam@SHYAM-PC>
2016-12-27 20:08:26 +0530

    Added UI for Forgot password.

---
+++ expensetrackerweb/src/main/webapp/expensetracker/app/view/login/ForgotPassword.js
@@ -0,0 +1,50 @@
+Ext.define('expensetracker.view.login.ForgotPassword', {
+	extend : 'Ext.window.Window',
+	xtype : 'forgotpwdwindow',
+	alias : 'view.forgotpwdwindow',
+	title : 'Forgot Password?',
+	layout : 'fit',
+	items : [ {
+		xtype : 'form',
+		reference : 'forgotpwdform',
+		platformConfig : {
+			desktop : {
+				width : 350
+			},
+
+			'!desktop' : {
+				width : '80%'
+			}
+		},
+		bodyPadding : 10,
+		layout : {
+			type : 'vbox',
+			align : 'stretch'
+		},
+		items : [ {
+			xtype : 'textfield',
+			fieldLabel : 'Your registered e-mail?',
+			allowBlank : false,
+			labelAlign : 'top',
+			reference : 'forgotpwdemail',
+			vtype : 'email',
+			validateBlank : true,
+			labelSeparator : ''
+		}, {
+			xtype : 'label',
+			padding : '10 10 10 10',
+			reference : 'forgotpwderrlbl',
+			style : {
+				color : 'red'
+			}
+		} ],
+		buttons : [ {
+			text : 'Send',
+			ui : 'soft-green',
+			formBind : true,
+			tooltip : 'Send',
+			iconCls : 'x-fa fa-envelope',
+			handler : 'onSendForgotPwd'
+		} ]
+	} ]
+});
