1ba8256d766e1931824db1155390c5c2754aa14f
tkmaed4 <tkmaed4@VGFCVIEW01450.cp.ad.kohls.com>
2017-06-13 17:05:22 +0530

    Added methods to get Window Height, Width, X, Y

--- expensetrackerweb/src/main/webapp/expensetracker/app/util/Constants.js
+++ expensetrackerweb/src/main/webapp/expensetracker/app/util/Constants.js
@@ -9,5 +9,29 @@ Ext.define('expensetracker.util.Constants', {
 		var me = this;
 		this.initConfig(config);
 		return me;
+	},
+	getWindowHeight : function() {	
+		if(!Ext.os.is.Windows) {
+			return Ext.Element.getViewportHeight();
+		}
+		return Ext.Element.getViewportHeight() - this.getToolbarHeight();	
+	},
+	getWindowWidth : function() {
+		if(!Ext.os.is.Windows) {
+			return Ext.Element.getViewportWidth();
+		}
+		return Ext.Element.getViewportWidth() - this.getNavBarWidth();
+	},
+	getWindowY : function() {
+		if(!Ext.os.is.Windows) {
+			return 0;
+		}
+		return this.getToolbarHeight();	
+	},
+	getWindowX : function() {
+		if(!Ext.os.is.Windows) {
+			return 0;
+		}
+		return this.getNavBarWidth();	
 	}
 });
